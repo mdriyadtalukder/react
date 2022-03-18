975d9ffcc03d311e70ca780ad41aadb55d7c3ca8
mdriyadtalukder <riyadtalukder67@gmail.com>
2022-03-18 23:08:02 +0600

    1st

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Cal></Cal> */}
+      <Phl></Phl>
     </div>
   );
 }
+
+function Phl() {
+  const [ar, Seta] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => Seta(data));
+  }, []);
+  return (
+    <div>
+      <h1>This is map</h1>
+      <div className="ck">
+      {
+          
+          ar.map(a => <Inf n={a}></Inf>)
+        
+     }
+      </div>
+    </div>
+  )
+}
+
+function Inf(props) {
+  const c={
+    color:'red'
+  }
+  return (
+    <div className='dv'>
+      <p>Name:{props.n.name}</p>
+      <p>UserName:{props.n.username}</p>
+      <p style={c}>Email:{props.n.email}</p>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Cal(){
+//   const [count,Setc]=useState(0);
+//   const cn=()=>{
+//     let c=count+1;
+//     Setc(c);
+//   }
+//   const cnl=()=>{
+//     let c=count-1;
+//     if(c<0){
+//     return;
+//     }
+//     Setc(c);
+//   }
+//   return (
+//     <div>
+//       <h1>{count}</h1>
+//       <button onClick={cn}>Add</button>
+//       <button onClick={cnl}>Sub</button>
+//     </div>
+//   )
+// }
 
 export default App;
